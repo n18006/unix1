3616acf4edcbd1516930bfb31b982113442bcc77
Kimiyuki Yamauchi <yamauchi@std.it-colleage.ac.jp>
2018-05-18 06:01:40 +0000

    5/17の課題

--- 0517/051703.sh
+++ 0517/051703.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 cd dir3
-touch file{01..10}
+touch file01
+touch file02
+touch file03
+touch file04
+touch file05
+touch file06
+touch file07
+touch file08
+touch file09
+touch file10
 
 
 
