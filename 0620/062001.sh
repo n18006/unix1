ef3d5b7d0c51c4fd3ba017f881e17af9088f80c0
Kimiyuki Yamauchi <yamauchi@std.it-colleage.ac.jp>
2018-06-20 01:38:58 +0000

    0620課題

---
+++ 0620/062001.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for i in $(seq 1 100)
+do
+	if [ $((i % 15)) -eq 0 ]; then
+	echo "FIZZBUZZ"
+
+elif [ $((i % 5)) -eq 0 ]; then
+	echo "BUZZ"
+
+elif [ $((i % 3)) -eq 0 ]; then
+	echo "FIZZ"
+
+else
+	echo $i
+
+fi
+
+done
